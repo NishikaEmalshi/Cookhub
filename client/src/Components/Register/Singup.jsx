7594a74651c50e97395cfb1d49be295bc5e165b6
NishikaEmalshi <nishikaemalshi@gmail.com>
2025-04-29 22:06:47 +0530

    commmit

--- client/src/Components/Register/Singup.jsx
+++ client/src/Components/Register/Singup.jsx
@@ -10,7 +10,7 @@ import {
 import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import { signupAction } from "../../Redux/Auth/Action";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 
@@ -28,131 +28,152 @@ const validationSchema = Yup.object().shape({
 });
 
 const Signup = () => {
-  const initialValues = { email: "", username: "", password: "", name:"" };
-  const dispatch=useDispatch();
-  const {auth}=useSelector(store=>store);
-const navigate=useNavigate();
-const toast = useToast();
-console.log("auth :-",auth.signup?.username)
+  const initialValues = { email: "", username: "", password: "", name: "" };
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+  const navigate = useNavigate();
+  const toast = useToast();
 
-  const handleSubmit = (values,actions) => {
-    dispatch(signupAction(values))
-    console.log("signup",values);
+  const handleSubmit = (values, actions) => {
+    dispatch(signupAction(values));
+    console.log("signup", values);
     actions.setSubmitting(false);
   };
 
-  useEffect(()=>{
-if(auth.signup?.username){
-  
-  navigate("/login")
-  toast({
-    title: 'Account created successfully',
-    status: 'success',
-    duration: 8000,
-    isClosable: true,
-  })
-}
-  },[auth.signup])
+  useEffect(() => {
+    if (auth.signup?.username) {
+      navigate("/login");
+      toast({
+        title: "Account created successfully",
+        status: "success",
+        duration: 8000,
+        isClosable: true,
+      });
+    }
+  }, [auth.signup]);
 
   return (
-    <div>
-        <div className="border border-slate-300 ">
-      <Box p={8} display="flex" flexDirection="column" alignItems="center">
-        <img
-          className="border border-red-800"
-          src="https://www.pekarskiglasnik.com/images/slike/cook%20hub%20kuhinja/CookHub-logo.JPG"
-          alt=""
-        />
-        <p className="font-bold opacity-50 text-lg mb-10 text-center">
-          Experience the delightful experience of cooking with CookHub.
-        </p>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={handleSubmit}
-          validationSchema={validationSchema}
-        >
-          {(formikProps) => (
-            <Form className="w-full">
-              <Field name="email">
-                {({ field, form }) => (
-                  <FormControl
-                    isInvalid={form.errors.email && form.touched.email}
-                    mb={4}
-                  >
-                    <Input
-                      className="w-full"
-                      {...field}
-                      id="email"
-                      placeholder="Mobile Number Or Email"
-                    />
-                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Field name="username">
-                {({ field, form }) => (
-                  <FormControl
-                    isInvalid={form.errors.username && form.touched.username}
-                    mb={4}
-                  >
-                    <Input {...field} id="username" placeholder="username" />
-                    <FormErrorMessage>{form.errors.username}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Field name="name">
-                {({ field, form }) => (
-                  <FormControl
-                    isInvalid={form.errors.name && form.touched.name}
-                    mb={4}
-                  >
-                    <Input {...field} id="name" placeholder="Full Name" />
-                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Field name="password">
-                {({ field, form }) => (
-                  <FormControl
-                    isInvalid={form.errors.password && form.touched.password}
-                    mb={4}
-                  >
-                    <Input
-                      {...field}
-                      type="password"
-                      id="password"
-                      placeholder="Password"
-                    />
-                    <FormErrorMessage>{form.errors.password}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <p className="text-center">
-              
-              </p>
-              <p className="mt-5 text-center">
-                By signing up, you agree to our Terms , Privacy Policy and
-                Cookies Policy .
-              </p>
-              <Button
-                className="w-full"
-                mt={4}
-                colorScheme="blue"
-                type="submit"
-                isLoading={formikProps.isSubmitting}
-              >
-                Sign Up
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </Box>
+    <div className="min-h-screen flex justify-center items-center">
+      <div
+  className="rounded-lg shadow-lg max-w-md w-full p-6"
+  style={{
+    backgroundColor: "rgba(255, 255, 255, 0.85)",
+    backdropFilter: "blur(5px)", 
+  }}
+>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <img
+            className="mb-4"
+            src="https://www.pekarskiglasnik.com/images/slike/cook%20hub%20kuhinja/CookHub-logo.JPG"
+            alt="CookHub Logo"
+          />
+          <p className="font-bold opacity-50 text-lg mb-6 text-center">
+            Experience the delightful experience of cooking with CookHub.
+          </p>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
+            {(formikProps) => (
+              <Form className="w-full">
+                <Field name="email">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.email && form.touched.email}
+                      mb={4}
+                    >
+                      <Input
+                        {...field}
+                        id="email"
+                        placeholder="Please Enter Your Email"
+                      />
+                      <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="username">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.username && form.touched.username}
+                      mb={4}
+                    >
+                      <Input
+                        {...field}
+                        id="username"
+                        placeholder="Please Enter Your Username"
+                      />
+                      <FormErrorMessage>
+                        {form.errors.username}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="name">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.name && form.touched.name}
+                      mb={4}
+                    >
+                      <Input
+                        {...field}
+                        id="name"
+                        placeholder="Please Enter Full Name"
+                      />
+                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="password">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.password && form.touched.password}
+                      mb={4}
+                    >
+                      <Input
+                        {...field}
+                        type="password"
+                        id="password"
+                        placeholder="Please enter Your Password"
+                      />
+                      <FormErrorMessage>
+                        {form.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <p className="mt-4 text-center text-sm">
+                  By signing up, you agree to our{" "}
+                  <span className="underline">Terms</span>,{" "}
+                  <span className="underline">Privacy Policy</span> and{" "}
+                  <span className="underline">Cookies Policy</span>.
+                </p>
+                <Button
+                  className="w-full"
+                  mt={4}
+                  colorScheme="blue"
+                  type="submit"
+                  isLoading={formikProps.isSubmitting}
+                >
+                  Sign Up
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+        <div className="w-full mt-5">
+          <p className="text-center py-2 text-sm">
+            If You Have Already Account{" "}
+            <span
+              onClick={() => navigate("/login")}
+              className="ml-2 text-blue-700 cursor-pointer"
+            >
+              Sign In
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
-    <div className="w-full border border-slate-300 mt-5">
-       <p className="text-center py-2">If You Have Already Account <span onClick={()=>navigate("/login")} className="ml-2 text-blue-700 cursor-pointer">Sign In</span></p>
-    </div>
-    </div>
-  
   );
 };
 
--- client/src/Config/UploadToCloudinary.js
+++ client/src/Config/UploadToCloudinary.js
@@ -12,9 +12,9 @@ export const uploadToCloudinary = async (file) => {
       data.append("upload_preset", "cloud_upload_img");
     }
     
-    data.append("cloud_name", "dusdvdo7q");
-    
-    const res = await fetch("https://api.cloudinary.com/v1_1/dusdvdo7q/upload", {
+    data.append("cloud_name", "dvawjogg8");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/dvawjogg8/upload", {
       method: "POST",
       body: data,
     });
