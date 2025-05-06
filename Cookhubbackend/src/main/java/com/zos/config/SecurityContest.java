85949f9ed67bf516a350a71ebb40c8a8e6aaef45
NishikaEmalshi <nishikaemalshi@gmail.com>
2025-05-06 15:53:19 +0530

    Secret Key fixesthe update to new one

--- Cookhubbackend/src/main/java/com/zos/config/SecurityContest.java
+++ Cookhubbackend/src/main/java/com/zos/config/SecurityContest.java
@@ -1,7 +1,7 @@
 package com.zos.config;
 
 public class SecurityContest {
-	
+	//JWT Secret Key set
 	public static final String JWT_KEY="zjdbfagvqruuyzbhgdfpeoajnebxiueban";
 	public static final String HEADER="Authorization";
 
